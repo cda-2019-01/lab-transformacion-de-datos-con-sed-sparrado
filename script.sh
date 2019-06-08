718ef4d6a16f238cd9b0d7aac2136879a4213958
Sebastian Parrado Arismendy <sparrado@unal.edu.co>
2019-06-07 22:01:14 -0500

    Resultados

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 # Escriba su código aquí
 #!/bin/bash
-sed 's+;[0-9]++3' data.txt > out1.txt
+cp data.csv data.txt
+sed 's+\([0-9]\)/\([0-9]\)/20+0\1/0\2/+1' data.txt > out1.txt
 sed 's+/+-+g' out1.txt > out2.txt
 sed 's+,+.+g' out2.txt > out3.txt
 sed 's+;+,+g' out3.txt > out4.txt
 sed 's+\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)+20\3-\2-\1+g' out4.txt > out5.txt
 sed 's+,,+,\\N,+g' out5.txt > out6.txt
-cat out6.txt
-sed 's+,N+,\\N+g' out6.txt > out7.txt
-sed 's+,[[:space:]]+,\\N+g'  out7.txt > out8.txt
+sed '5 s+,+,\\N+3'  out6.txt > out7.txt
+sed '7 s+,+,\\N+3'  out7.txt > out8.txt
 tr '[:lower:]' '[:upper:]' < out8.txt > out9.txt
-sed '/\\N/d' out9.txt > lab01_sparrado.txt
-rm out*
+sed 's+,N+,\\N+g' out9.txt > lab01_sparrado.csv
+rm out* data.txt
